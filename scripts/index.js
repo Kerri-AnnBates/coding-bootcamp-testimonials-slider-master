8ae3a7183da8ad347cac2e5666e48fd0dbfa396c
Kerri-Ann <kaybee1jam@yahoo.com>
2020-09-27 11:41:22 -0400

    fix button directions

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+let nextButton = document.querySelector(".btn__next");
+let prevButton = document.querySelector(".btn__prev");
+
+nextButton.addEventListener("click", () => {
+	console.log("next");
+});
+prevButton.addEventListener("click", () => {
+	console.log("previous");
+});
