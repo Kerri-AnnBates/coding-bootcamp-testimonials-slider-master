3a5b7e24de642c581c648849cb8cb2f48a4103bb
Kerri-Ann <kaybee1jam@yahoo.com>
2020-09-27 11:54:46 -0400

    get list of elements to iterate over

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,6 @@
 let nextButton = document.querySelector(".btn__next");
 let prevButton = document.querySelector(".btn__prev");
+let imageList = document.querySelector(".profile__image__list");
+let contentList = document.querySelector(".profile__content__list");
 
-nextButton.addEventListener("click", () => {
-	console.log("next");
-});
-prevButton.addEventListener("click", () => {
-	console.log("previous");
-});
+console.log(imageList.children);
