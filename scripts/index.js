cf14d016456d9db7bb9f59ea7e78d66c5c34be6b
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-03 16:39:40 -0400

    cleanup

--- scripts/index.js
+++ scripts/index.js
@@ -8,27 +8,7 @@ let profileIndex = 0;
 let currentImage, prevImage, nextImage;
 let content;
 
-
-const getImagePositions = () => {
-	// check if at end or beg of array to reset classes
-	if (profileIndex == 0 || profileIndex == imageList.length - 1) {
-		if (profileIndex == 0) {
-			prevImage = imageList[imageList.length - 1];
-			nextImage = imageList[profileIndex + 1];
-
-		} else {
-			prevImage = imageList[profileIndex - 1];
-			nextImage = imageList[profileIndex];
-		}
-
-	} else {
-		prevImage = imageList[profileIndex - 1];
-		nextImage = imageList[profileIndex + 1];
-	}
-}
-
 const resetStyles = () => {
-
 	[...imageList].forEach(img => {
 		img.classList.remove("active");
 	});
