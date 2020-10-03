1a07b33d18158c8b2fe5facebec4d47af657de14
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-03 13:19:40 -0400

    complete standard change of profile

--- scripts/index.js
+++ scripts/index.js
@@ -5,32 +5,15 @@ const contentList = document.querySelector(".profile__content-list");
 
 // Flag set to 0
 let profileIndex = 0;
+let currentImage, prevImage, nextImage;
+let content;
 
-nextButton.addEventListener("click", () => {
-	let currentImage, prevImage, nextImage;
-	let content = contentList.children[profileIndex];
 
+const showNext = () => {
+	content = contentList.children[profileIndex];
 	currentImage = imageList.children[profileIndex];
 
-	// check if at end or beg of array to reset classes
-	if (profileIndex == 0 || profileIndex == imageList.children.length - 1) {
-		if (profileIndex == 0) {
-			prevImage = imageList.children[imageList.children.length - 1];
-			nextImage = imageList.children[profileIndex + 1];
-
-		} else {
-			prevImage = imageList.children[profileIndex - 1];
-			nextImage = imageList.children[profileIndex];
-		}
-
-	} else {
-		prevImage = imageList.children[profileIndex - 1];
-		nextImage = imageList.children[profileIndex + 1];
-	}
-
-	currentImage.classList.remove("active", "slide-left", "slide-right");
-	prevImage.classList.remove("active", "slide-left", "slide-right");
-	nextImage.classList.remove("active", "slide-left", "slide-right");
+	currentImage.classList.remove("active");
 	content.classList.remove("active");
 
 	if (profileIndex === imageList.children.length - 1) {
@@ -39,35 +22,17 @@ nextButton.addEventListener("click", () => {
 		profileIndex += 1;
 	}
 
-	// check if at end or beg of array to add classes
-	if (profileIndex == 0 || profileIndex == imageList.children.length - 1) {
-		if (profileIndex == 0) {
-			prevImage = imageList.children[imageList.children.length - 1];
-			nextImage = imageList.children[profileIndex + 1];
-
-		} else {
-			prevImage = imageList.children[profileIndex - 1];
-			nextImage = imageList.children[profileIndex];
-		}
-
-	} else {
-		prevImage = imageList.children[profileIndex - 1];
-		nextImage = imageList.children[profileIndex + 1];
-	}
-
-
 	currentImage = imageList.children[profileIndex];
 	content = contentList.children[profileIndex];
 	currentImage.classList.add("active");
-	prevImage.classList.add("slide-left");
 	content.classList.add("active");
-});
+}
 
-prevButton.addEventListener("click", () => {
-	let image = imageList.children[profileIndex];
-	let content = contentList.children[profileIndex];
+const showPrev = () => {
+	content = contentList.children[profileIndex];
+	currentImage = imageList.children[profileIndex];
 
-	image.classList.remove("active");
+	currentImage.classList.remove("active");
 	content.classList.remove("active");
 
 	if (profileIndex === 0) {
@@ -76,8 +41,11 @@ prevButton.addEventListener("click", () => {
 		profileIndex -= 1;
 	}
 
-	image = imageList.children[profileIndex];
+	currentImage = imageList.children[profileIndex];
 	content = contentList.children[profileIndex];
-	image.classList.add("active");
+	currentImage.classList.add("active");
 	content.classList.add("active");
-});
+}
+
+nextButton.addEventListener("click", showNext);
+prevButton.addEventListener("click", showPrev);
