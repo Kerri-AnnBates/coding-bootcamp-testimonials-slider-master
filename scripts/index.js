340899dbb69fa101ac185f50ca937cb7deaeacea
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-03 13:41:00 -0400

    minor clean up

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const nextButton = document.querySelector(".btn__next");
 const prevButton = document.querySelector(".btn__prev");
-const imageList = document.querySelector(".profile__image-list");
-const contentList = document.querySelector(".profile__content-list");
+const imageList = document.querySelector(".profile__image-list").children;
+const contentList = document.querySelector(".profile__content-list").children;
 
 // Flag set to 0
 let profileIndex = 0;
@@ -9,40 +9,66 @@ let currentImage, prevImage, nextImage;
 let content;
 
 
-const showNext = () => {
-	content = contentList.children[profileIndex];
-	currentImage = imageList.children[profileIndex];
+const getImagePositions = () => {
+	// check if at end or beg of array to reset classes
+	if (profileIndex == 0 || profileIndex == imageList.length - 1) {
+		if (profileIndex == 0) {
+			prevImage = imageList[imageList.length - 1];
+			nextImage = imageList[profileIndex + 1];
 
-	currentImage.classList.remove("active");
+		} else {
+			prevImage = imageList[profileIndex - 1];
+			nextImage = imageList[profileIndex];
+		}
+
+	} else {
+		prevImage = imageList[profileIndex - 1];
+		nextImage = imageList[profileIndex + 1];
+	}
+}
+
+const resetStyles = () => {
+
+	[...imageList].forEach(img => {
+		img.classList.remove("active");
+	});
 	content.classList.remove("active");
+}
 
-	if (profileIndex === imageList.children.length - 1) {
+const showNext = () => {
+	content = contentList[profileIndex];
+	currentImage = imageList[profileIndex];
+
+	resetStyles();
+
+	if (profileIndex === imageList.length - 1) {
 		profileIndex = 0;
 	} else {
 		profileIndex += 1;
 	}
 
-	currentImage = imageList.children[profileIndex];
-	content = contentList.children[profileIndex];
+	currentImage = imageList[profileIndex];
+	content = contentList[profileIndex];
+
 	currentImage.classList.add("active");
 	content.classList.add("active");
 }
 
 const showPrev = () => {
-	content = contentList.children[profileIndex];
-	currentImage = imageList.children[profileIndex];
+	content = contentList[profileIndex];
+	currentImage = imageList[profileIndex];
 
-	currentImage.classList.remove("active");
-	content.classList.remove("active");
+	resetStyles();
 
 	if (profileIndex === 0) {
-		profileIndex = imageList.children.length - 1;
+		profileIndex = imageList.length - 1;
 	} else {
 		profileIndex -= 1;
 	}
 
-	currentImage = imageList.children[profileIndex];
-	content = contentList.children[profileIndex];
+	currentImage = imageList[profileIndex];
+	content = contentList[profileIndex];
+
 	currentImage.classList.add("active");
 	content.classList.add("active");
 }
